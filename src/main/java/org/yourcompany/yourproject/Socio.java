2ac1e3a7446bbd791fcf34961ce850f592cfc677
danielramirezzz <daniel.ramirez2@estudiantesunibague.edu.co>
2026-02-10 17:46:24 -0500

    Clase socio creada

---
+++ src/main/java/org/yourcompany/yourproject/Socio.java
@@ -0,0 +1,24 @@
+package org.yourcompany.yourproject;
+
+public class Socio extends Usuario {
+    private int limiteEmjemplares;
+    private int ejemplaresEnPrestamo;
+    private double multasPendientes;
+
+    public Socio(String id, String nombre, String email, String turno, int limiteEmjemplares) {
+        super(id, nombre, email);
+        this.limiteEmjemplares = limiteEmjemplares;
+        ejemplaresEnPrestamo = 0;
+        multasPendientes = 0;
+    }
+
+    public void registrarPrestamo (int cantidad){
+        if (cantidad > (limiteEmjemplares - ejemplaresEnPrestamo)){
+            throw new IllegalArgumentException("Supera el límite del socio");
+        }
+        if (multasPendientes > 0){
+            throw new IllegalArgumentException("No se puede pedir, tiene multa");
+        }
+        System.out.println("Se prestaron" + cantidad + "libros");
+    }
+}
