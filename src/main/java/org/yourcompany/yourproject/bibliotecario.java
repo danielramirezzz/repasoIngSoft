604dc6169b2cc0a7734a4eb879164b91a7062dcc
danielramirezzz <daniel.ramirez2@estudiantesunibague.edu.co>
2026-02-10 17:19:34 -0500

    Clase bibliotecario creada

--- src/main/java/org/yourcompany/yourproject/bibliotecario.java
+++ src/main/java/org/yourcompany/yourproject/bibliotecario.java
@@ -5,7 +5,7 @@ public class bibliotecario extends  usuario{
 
     private String turno;
 
-    public bibliotecario(String id, String nombre, String email) {
+    public bibliotecario(String id, String nombre, String email, String turno) {
         super(id, nombre, email);
         this.turno = turno;
     }
